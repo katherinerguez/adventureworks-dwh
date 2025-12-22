a202514248d925803f9ec56757a5da72342d458c
Katherine <rkatherine538@gmail.com>
2025-12-21 22:41:58 -0500

    Changes to the configuration and add the data catalog

--- aw-dwh/config/aio/entrypoint.sh
+++ aw-dwh/config/aio/entrypoint.sh
@@ -21,5 +21,30 @@ if [[ $# -gt 0 ]] ; then
     eval "$1"
 fi
 
+!/bin/bash
 
+# export DAGSTER_DIR=/var/lib/adventureworks/dagster
+# export DAGSTER_HOM=/opt/dagster
+# export SPARK_DIR=/opt/spark
+
+# # Instalar Dagster primero (agregar estas líneas)
+# pip install dagster dagster-webserver pandas pyarrow
+
+# # Crear directorio si no existe
+# mkdir -p ${DAGSTER_DIR}
+
+# # Iniciar Dagster
+# cd ${DAGSTER_DIR} && dagster dev -p 3070 -h 0.0.0.0 &
+
+# # Iniciar servicios de Spark
+# cd ${SPARK_DIR}
+# start-master.sh -p 7077
+# start-worker.sh spark://spark-iceberg:7077
+# start-history-server.sh
+# start-thriftserver.sh --driver-java-options "-Dderby.system.home=/tmp/derby"
+
+# # Entrypoint
+# if [[ $# -gt 0 ]] ; then
+#     eval "$1"
+# fi
 
